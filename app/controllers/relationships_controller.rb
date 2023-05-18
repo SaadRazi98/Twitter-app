f46826f0e3c1c652c761ea87f9514a1e1e6e24d7
SAAD <SAADRAZI1998@GMAIL.com>
2023-05-18 15:31:01 -0500

    Add create action

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -5,5 +5,12 @@ class RelationshipsController < ApplicationController
     render :index
   end 
 
+  def create 
+    @relationship = relationship.create(
+      follower_id: params[:follower_id],
+      leader_id: params[:leader_id],
+    )
+  end 
+  render :show
   
 end
